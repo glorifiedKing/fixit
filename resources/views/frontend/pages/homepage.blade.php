c2d28e41d45ca5808f39e7eacbb90e048de36ef3
Raymond <rayim25@gmil.com>
2019-08-28 00:00:02 -0700

    homepage links

--- resources/views/frontend/pages/homepage.blade.php
+++ resources/views/frontend/pages/homepage.blade.php
@@ -10,25 +10,25 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-sm">
+        <div class="col-md-3 col-sm-6 options-wrapper">
           <a href="{{url('/login')}}">Fundi Wallet</a>
         </div>
-        <div class="col-md-3 col-sm-6">
+        <div class="col-md-3 col-sm-6 options-wrapper">
           <a href="{{url('/professionals')}}">Professionals</a>
         </div>
-        <div class="col-md-3 col-sm-6">
+        <div class="col-md-3 col-sm-6 options-wrapper">
           <a href="{{url('/')}}">Buy from the store</a>
         </div>
-        <div class="col-md-3 col-sm-6">
+        <div class="col-md-3 col-sm-6 options-wrapper">
           <a href="{{url('/')}}">Get Financed</a>
         </div>
-        <div class="col-md-3 col-sm-6">
+        <div class="col-md-3 col-sm-6 options-wrapper">
           <a href="{{url('/')}}">Get a building plan</a>
         </div>
-        <div class="col-md-3 col-sm-6">
+        <div class="col-md-3 col-sm-6 options-wrapper">
           <a href="{{url('/')}}">Insure your project</a>
         </div>
-        <div class="col-md-3 col-sm-6">
+        <div class="col-md-3 col-sm-6 options-wrapper">
           <a href="{{url('/')}}">Utilities</a>
         </div>
 
